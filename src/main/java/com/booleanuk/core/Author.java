bdf4c1c97bfacb14563ed8f7f6f0fec156d880c0
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-24 11:29:57 +0100

    Added Author class

---
+++ src/main/java/com/booleanuk/core/Author.java
@@ -0,0 +1,13 @@
+package com.booleanuk.core;
+
+public class Author {   //(name, contact information, website
+    String name;
+    String contactInformation;
+    String website;
+
+    public Author(String name, String contactInformation, String website) {
+        this.name = name;
+        this.contactInformation = contactInformation;
+        this.website = website;
+    }
+}
